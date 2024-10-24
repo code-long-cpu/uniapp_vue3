ca342791eaa1ba97f49436ad1d0c3c4150b1c27f
long <longbao7552@gmail.com>
2024-10-23 17:49:36 -0700

    pets

--- pages/requestDemo/requestDemo.vue
+++ pages/requestDemo/requestDemo.vue
@@ -1,21 +1,49 @@
 <template>
 	<view class="container">
+		<view class="menu">
+			  <uni-segmented-control :current="current" :values="value" @clickItem="onClickItem" styleType="button" activeColor="#1ce5e0"></uni-segmented-control>
+		</view>
 		<view class="layout">
 			<view class="box" v-for="(item,index) in pets" :key="item._id" @click ="preview(index)">
 				<view class="pic">
 					<image lazy-load :src="item.url" mode="widthFix"></image>
+					<view class="message">
+						<view class="up">
+							<text>{{item.source}}</text>
+							<text>{{item.type}}</text>
+						</view>
+						<view class="bellow">
+							<text>第 {{index+1}} 张</text>
+							<!-- <view>{{index}}</view> -->
+						</view>
+					</view>
 				</view>
 				<view class="text">{{item.content}}</view>
 				<view class="author">———— {{item.author}}</view>
 			</view>
 		</view>
+		<view class="float">
+			<view class="item" @click="reFresh">
+				<uni-icons type="refreshempty" size="30" color="#888"></uni-icons>
+			</view>
+			<view class="item" @click="backTop">
+				<uni-icons type="arrow-up" size="30" color="#888"></uni-icons>
+			</view>
+		</view>
+		<view class="loadMore">
+			<uni-load-more status="loading"></uni-load-more>
+		</view>
 	</view>
 </template>
 
 <script setup>
-	import {ref} from 'vue'
+	import {computed, ref} from 'vue'
 	import {onReachBottom,onPullDownRefresh} from '@dcloudio/uni-app'
 	const pets = ref([])
+	const current=ref(0)
+	// 全部狗猫分类数据源
+	const classify = [{key:'all',value:'全部'},{key:'dog',value:'狗狗'},{key:'cat',value:'猫猫'}]
+	const value = computed(()=>{return classify.map(item=>item.value)}) //['全部', '狗狗', '猫猫']
 	
 	// 展开运算符
 	let arr1 = [1,2,3]
@@ -31,18 +59,22 @@
 		 try{
 			 const res = await uni.request({
 			 	url:"https://tea.qingnian8.com/tools/petShow",
+			 	// url:"https://tea.qingnian8.com/tools/taoShow",
 			 	// url:"https://api.thecatapi.com/v1/images/search?limit=1",
 			 	method:'GET',
 			 	data:{
 			 		size:5,	
 			 		// limit:10,	
+					type:classify[current.value].key
 			 	},
+				
 			 	header:{
 			 		"access-key":"369395"
 			 	}
 			 })
 			 if(res.data.errCode===0){
 				 // pets.value = res.data.data
+				 // console.log(res.data.data)
 				 // 展开运算符,追加新数据给旧数据
 				 pets.value = [...pets.value,...res.data.data]
 			 }else{
@@ -55,7 +87,7 @@
 		 }catch(error){
 			 console.log(error)
 			 uni.showToast({
-				 title:'请求有误',
+				 title:error.errMsg,
 				 icon:'none',
 				 duration:3000
 			 })
@@ -94,25 +126,76 @@
 		request()
 	})
 	
+	// 点击刷新
+	const reFresh = ()=>{
+		console.log("刷新")
+		uni.startPullDownRefresh()
+	}
+	
+	// 回到顶部
+	const backTop=()=>{
+		console.log("回到顶部")
+		uni.pageScrollTo({
+			scrollTop:0,
+			duration:100
+		})
+	}
+	
+	// 分段器
+	const onClickItem=(e)=>{
+		// console.log(e.currentIndex)
+		// e参数可传不到request中
+		current.value=e.currentIndex
+		// console.log(classify[current.value].key)
+		pets.value=[]
+		request()
+	}
+	
+	
 </script>
 
 <style lang="scss" scoped>
 .container{
-	.layout{
+	.menu{
 		padding:50rpx;
+	}
+	.layout{
+		padding:0 50rpx;
 		// border:solid 1px black;
-		.box{
+		.box{					
 			margin-bottom:60rpx;
-			box-shadow:0 10rpx 30rpx rgba(0,0,0,0.08);
+			box-shadow:0 10rpx 30rpx rgba(0,0,0,0.3);
 			// border:solid 1px black;
 			border-radius: 15rpx;
 			overflow:hidden;
 			.pic{
 				// border:solid 1px orange;
-				overflow:hidden;
+				position:relative;
+				// overflow:hidden;
 				image{
 					width:100%;
 					// border:solid 1px red;
+				}
+				.message{
+					padding:40rpx;
+					font-size: 40rpx;
+					color:white;
+					// border:solid 1px red;
+					position:absolute;
+					top:0rpx;
+					width:100%;
+					height:100%;
+					display:flex;
+					flex-direction: column;
+					justify-content: space-between;
+					.up,.bellow{
+						display:flex;
+						justify-content: space-between;
+						text{
+							text-shadow: 0.1em 0.1em #333
+						}
+					}
+					
 				}
 			}
 			.text{
@@ -131,6 +214,26 @@
 			}
 			
 		}
+	}
+	.float{
+		position:fixed;
+		right:50rpx;
+		bottom:10rpx;
+		padding-bottom: calc(env(safe-area-inset-bottom) + 30rpx);
+		// border:solid 1px black;
+		.item{
+			width:100rpx;
+			height:100rpx;
+			background-color: white;
+			border-radius: 50%;
+			display: flex; 
+			justify-content: center;
+			align-items: center;
+			margin-bottom:10rpx;
+		}
+	}
+	.loadMore{
+		padding-bottom: calc(env(safe-area-inset-bottom) + 30rpx);
 	}
 }
   
--- unpackage/dist/dev/mp-weixin/pages/requestDemo/requestDemo.js
+++ unpackage/dist/dev/mp-weixin/pages/requestDemo/requestDemo.js
@@ -1,9 +1,26 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+if (!Array) {
+  const _easycom_uni_segmented_control2 = common_vendor.resolveComponent("uni-segmented-control");
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_load_more2 = common_vendor.resolveComponent("uni-load-more");
+  (_easycom_uni_segmented_control2 + _easycom_uni_icons2 + _easycom_uni_load_more2)();
+}
+const _easycom_uni_segmented_control = () => "../../uni_modules/uni-segmented-control/components/uni-segmented-control/uni-segmented-control.js";
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_load_more = () => "../../uni_modules/uni-load-more/components/uni-load-more/uni-load-more.js";
+if (!Math) {
+  (_easycom_uni_segmented_control + _easycom_uni_icons + _easycom_uni_load_more)();
+}
 const _sfc_main = {
   __name: "requestDemo",
   setup(__props) {
     const pets = common_vendor.ref([]);
+    const current = common_vendor.ref(0);
+    const classify = [{ key: "all", value: "全部" }, { key: "dog", value: "狗狗" }, { key: "cat", value: "猫猫" }];
+    const value = common_vendor.computed(() => {
+      return classify.map((item) => item.value);
+    });
     let arr1 = [1, 2, 3];
     let arr2 = [4, 5, 6];
     console.log([...arr1, ...arr2]);
@@ -12,11 +29,13 @@ const _sfc_main = {
       try {
         const res = await common_vendor.index.request({
           url: "https://tea.qingnian8.com/tools/petShow",
+          // url:"https://tea.qingnian8.com/tools/taoShow",
           // url:"https://api.thecatapi.com/v1/images/search?limit=1",
           method: "GET",
           data: {
-            size: 5
+            size: 5,
             // limit:10,	
+            type: classify[current.value].key
           },
           header: {
             "access-key": "369395"
@@ -34,7 +53,7 @@ const _sfc_main = {
       } catch (error) {
         console.log(error);
         common_vendor.index.showToast({
-          title: "请求有误",
+          title: error.errMsg,
           icon: "none",
           duration: 3e3
         });
@@ -59,16 +78,57 @@ const _sfc_main = {
       pets.value = [];
       request();
     });
+    const reFresh = () => {
+      console.log("刷新");
+      common_vendor.index.startPullDownRefresh();
+    };
+    const backTop = () => {
+      console.log("回到顶部");
+      common_vendor.index.pageScrollTo({
+        scrollTop: 0,
+        duration: 100
+      });
+    };
+    const onClickItem = (e) => {
+      current.value = e.currentIndex;
+      pets.value = [];
+      request();
+    };
     return (_ctx, _cache) => {
       return {
-        a: common_vendor.f(pets.value, (item, index, i0) => {
+        a: common_vendor.o(onClickItem),
+        b: common_vendor.p({
+          current: current.value,
+          values: value.value,
+          styleType: "button",
+          activeColor: "#1ce5e0"
+        }),
+        c: common_vendor.f(pets.value, (item, index, i0) => {
           return {
             a: item.url,
-            b: common_vendor.t(item.content),
-            c: common_vendor.t(item.author),
-            d: item._id,
-            e: common_vendor.o(($event) => preview(index), item._id)
+            b: common_vendor.t(item.source),
+            c: common_vendor.t(item.type),
+            d: common_vendor.t(index + 1),
+            e: common_vendor.t(item.content),
+            f: common_vendor.t(item.author),
+            g: item._id,
+            h: common_vendor.o(($event) => preview(index), item._id)
           };
+        }),
+        d: common_vendor.p({
+          type: "refreshempty",
+          size: "30",
+          color: "#888"
+        }),
+        e: common_vendor.o(reFresh),
+        f: common_vendor.p({
+          type: "arrow-up",
+          size: "30",
+          color: "#888"
+        }),
+        g: common_vendor.o(backTop),
+        h: common_vendor.p({
+          status: "loading"
         })
       };
     };
